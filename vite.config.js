08a1cd9547c44730d978501160cc61336605ac76
Salman <salman.hafizh1305@gmail.com>
2025-05-16 09:41:16 +0700

    fix: build vercel

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: 'dist',
     sourcemap: true,
     rollupOptions: {
       output: {
@@ -26,7 +27,11 @@ export default defineConfig({
           react: ['react', 'react-dom'],
           redux: ['redux', 'react-redux', 'redux-thunk'],
         },
+        format: 'es',
       },
     },
+  },
+  resolve: {
+    // Add any path aliases here if needed
   },
 });
